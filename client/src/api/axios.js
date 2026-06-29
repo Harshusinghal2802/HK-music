083183c29598e2fc0e22cf38d0a0dc54a49426e7
Harshusinghal2802 <harshsinghal2820@gmail.com>
2026-06-29 20:00:56 +0530

    Update project

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,17 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://hk-music-1.onrender.com',
+  baseURL: "https://hk-music.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
-export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL = "https://hk-music.onrender.com";
 
 export default api;
